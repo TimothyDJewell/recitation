e7754a86ba284b3157cb439a6a93cf1b8c78c7f2
Timothy Jewell <timothydjewell@gmail.com>
2023-07-10 13:50:23 -0700

    make hidden word size dynamic

--- src/MemoryBox.tsx
+++ src/MemoryBox.tsx
@@ -1,10 +1,31 @@
 import { useState } from "react";
 import Style from './MemoryBox.module.css';
 
+const widthClassNames = [
+    Style.w12, // default for length zero
+    Style.w1,
+    Style.w2,
+    Style.w3,
+    Style.w4,
+    Style.w5,
+    Style.w6,
+    Style.w7,
+    Style.w8,
+    Style.w9,
+    Style.w10,
+    Style.w11,
+    Style.w12,
+];
+
 const MemoryBox = (props: { hiddenText: string }) => {
     const [isExposed, updateIsExposed] = useState(false);
+    const className = [
+        Style.MemoryBox,
+        isExposed ? Style.Reveal : Style.Secret,
+        widthClassNames[props.hiddenText.length] || widthClassNames[widthClassNames.length - 1],
+    ].join(' ');
     return (
-        <span className={`${Style.MemoryBox} ${(isExposed ? Style.Reveal : Style.Secret)}`} onClick={() => updateIsExposed(!isExposed)}>
+        <span className={className} onClick={() => updateIsExposed(!isExposed)}>
             {isExposed ? props.hiddenText : '?'}
         </span>
     );
