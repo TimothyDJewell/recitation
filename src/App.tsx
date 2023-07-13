d2170b05618c2eff44206f194bfb83d4eba277ce
Timothy Jewell <timothydjewell@gmail.com>
2023-07-12 17:18:05 -0700

    initial work on typing memorization input

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,40 @@ const hideWords = (text: string, wordPredicate: (value: string, index: number) =
   return splitText.map((value, index) => wordIndices[index] === undefined || wordPredicate(value, wordIndices[index]!) ? value : <MemoryBox key={index} hiddenText={value} />);
 }
 
+const findIndexOfFirstNonMatch = (a: string, b: string) => {
+  let i = 0;
+  for (; i < a.length && i < b.length; ++i) {
+    if (a[i] !== b[i]) {
+      return i;
+    }
+  }
+  if (a.length !== b.length) {
+    return i;
+  }
+
+  return -1;
+}
+
+const TextTypingInput = ({ text }: { text: string }) => {
+  const [typed, setTyped] = useState('');
+  const indexOfNonMatch = findIndexOfFirstNonMatch(text, typed);
+  const matchedText = indexOfNonMatch > 0 ? text.substring(0, indexOfNonMatch) : '';
+  const unmatchedText = indexOfNonMatch !== -1 ? typed.substring(indexOfNonMatch) : '';
+  const remainingText = text.substring(matchedText.length);
+  return (
+    <>
+      <div>
+        <div style={{whiteSpace: 'pre'}}>
+          <span style={{ color: 'black' }}>{matchedText}</span>
+          <span style={{ color: 'red' }}>{unmatchedText}</span>
+          <span style={{ color: 'gray' }}>{remainingText}</span>
+        </div>
+        <textarea value={typed} onChange={e => setTyped(e.target.value)}/>
+      </div>
+    </>
+  );
+};
+
 const AppBody = () => {
   const [memorizationText, setMemorizationText] = useState(initialInput);
   const [hideMode, setHideMode] = useState({ mod: 3, n: 2 });
@@ -46,6 +80,7 @@ const AppBody = () => {
         <button onClick={decreaseMod}>Show fewer words</button>
       </div>
       <p style={{whiteSpace: 'pre'}}>{hideWords(memorizationText, (_, i) => i % hideMode.mod !== hideMode.n)}</p>
+      <TextTypingInput text={memorizationText}/>
     </div>
   );
 };
