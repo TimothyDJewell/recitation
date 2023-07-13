cfc9259f872d39a1401ae94ef2cd26674af35941
Timothy Jewell <timothydjewell@gmail.com>
2023-07-13 11:24:03 -0700

    fix whitespace handling in typing input
    Whitespace values are now normalized and considered equivalent, so a
    user can type newline or a space, and have it match the original text,
    regardless of if the original text used newlines or spaces.

--- src/TextTypingInput.tsx
+++ src/TextTypingInput.tsx
@@ -1,24 +1,45 @@
 import React, { useState } from 'react';
 
-const findIndexOfFirstNonMatch = (a: string, b: string) => {
-  let i = 0;
-  for (; i < a.length && i < b.length; ++i) {
-    if (a[i] !== b[i]) {
-      return i;
+/**
+ * Compares two strings to find the index of the first difference.
+ * Since whitespace of any form is considered equivalent, the indices
+ * for each string are both returned.
+ * @param a The first string to compare.
+ * @param b The second string to compare.
+ * @returns The indices in each string of the first difference.
+ */
+const findIndicesOfFirstDiff = (a: string, b: string) => {
+  let indexA = 0, indexB = 0;
+  while (indexA < a.length && indexB < b.length) {
+    if (a[indexA] === b[indexB]) {
+      indexA++;
+      indexB++;
+      continue;
     }
-  }
-  if (a.length !== b.length) {
-    return i;
+    const isAWhitespace = /\s/.test(a[indexA]);
+    const isBWhitespace = /\s/.test(b[indexB]);
+    if (isAWhitespace && isBWhitespace) {
+      indexA++;
+      indexB++;
+      continue;
+    } else if (isAWhitespace && /\s/.test(b[indexB - 1])) {
+      indexA++;
+      continue;
+    } else if (isBWhitespace && /\s/.test(a[indexA - 1])) {
+      indexB++;
+      continue;
+    }
+    return [indexA, indexB];
   }
 
-  return -1;
+  return [indexA, indexB];
 };
 
 const TextTypingInput = ({ text }: { text: string }) => {
   const [typed, setTyped] = useState('');
-  const indexOfNonMatch = findIndexOfFirstNonMatch(text, typed);
-  const matchedText = indexOfNonMatch > 0 ? text.substring(0, indexOfNonMatch) : '';
-  const unmatchedText = indexOfNonMatch !== -1 ? typed.substring(indexOfNonMatch) : '';
+  const [indexText, indexTyped] = findIndicesOfFirstDiff(text, typed);
+  const matchedText = text.substring(0, indexText);
+  const unmatchedText = typed.substring(indexTyped);
   const remainingText = text.substring(matchedText.length);
   return (
     <>
