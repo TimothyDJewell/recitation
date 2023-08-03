47c25d7156f7b1c20d2af9ac84b4cd5223fb5d8f
Timothy Jewell <timothydjewell@gmail.com>
2023-08-02 18:50:07 -0700

    add test for typing input

---
+++ src/TextTypingInput.test.tsx
@@ -0,0 +1,37 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import TextTypingInput from './TextTypingInput';
+
+test('renders the text as the remaining text', () => {
+  const text = 'Some test text';
+  const ui = render(<div data-testid="test"><TextTypingInput text={text} /></div>);
+  const spans = screen.getByTestId("test").getElementsByTagName('span');
+  expect(spans).toHaveLength(3);
+  expect(spans[0]).toHaveClass('matchedText');
+  expect(spans[0]).toBeEmptyDOMElement();
+  expect(spans[1]).toHaveClass('unmatchedText');
+  expect(spans[1]).toBeEmptyDOMElement();
+  expect(spans[2]).toHaveClass('remainingText');
+  expect(spans[2]).toHaveTextContent(text, { normalizeWhitespace: false });
+});
+
+[
+  { input: 'So', expected: { matched: /^So$/, unmatched: /^$/, remaining: /^me test text$/ } },
+  { input: 'Some', expected: { matched: /^Some$/, unmatched: /^$/, remaining: /^ test text$/ } },
+  { input: 'Some ', expected: { matched: /^Some $/, unmatched: /^$/, remaining: /^test text$/ } },
+  { input: 'Some wrong', expected: { matched: /^Some $/, unmatched: /^wrong$/, remaining: /^test text$/ } },
+].forEach(testcase => test(`renders the expected text when '${testcase.input}' is typed`, async () => {
+  const { input: inputValue, expected } = testcase;
+  const text = 'Some test text';
+  render(<div data-testid="test"><TextTypingInput text={text} /></div>);
+
+  fireEvent.change((await screen.findAllByRole('textbox'))[0], { target: { value: inputValue } });
+
+  const spans = screen.getByTestId("test").getElementsByTagName('span');
+  expect(spans).toHaveLength(3);
+  expect(spans[0]).toHaveClass('matchedText');
+  expect(spans[0]).toHaveTextContent(expected.matched, { normalizeWhitespace: false });
+  expect(spans[1]).toHaveClass('unmatchedText');
+  expect(spans[1]).toHaveTextContent(expected.unmatched, { normalizeWhitespace: false })
+  expect(spans[2]).toHaveClass('remainingText');
+  expect(spans[2]).toHaveTextContent(expected.remaining, { normalizeWhitespace: false });
+}));
